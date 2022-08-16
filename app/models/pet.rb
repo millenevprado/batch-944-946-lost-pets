c1533f40b33119f40cdf4aaa402478c6373eaa8e
Millene Prado <millene_vprado@hotmail.com>
2022-08-16 18:51:14 -0300

    add found days ago method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,9 @@ class Pet < ApplicationRecord
   SPECIES = ['dog', 'cat', 'bird', 'rabbit']
   validates :name, presence: true
   validates :species, inclusion: { in: SPECIES }
+
+  def found_days_ago
+    days_ago = (Date.today - found_on).to_i
+    "Found #{days_ago} #{days_ago == 1 ? 'day' : 'days'} ago"
+  end
 end
